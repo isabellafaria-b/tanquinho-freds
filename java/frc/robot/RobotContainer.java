500beb3a27a77caf2fe53831af08d0abf3912b2b
Bella .ᐟ <bella.ifb.2020@gmail.com>
2026-09-09 13:46:30 -0300

    Add files via upload

--- java/frc/robot/RobotContainer.java
+++ java/frc/robot/RobotContainer.java
@@ -1,20 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
   private final Joystick fred = new Joystick(0);
+  private final DriveSubsystem DriveSubsystem= new DriveSubsystem();
 
   public RobotContainer() {
-    
-    configureBindings();
+    DriveSubsystem();
   }
 
-  private void configureBindings() {
-  
+  private void DriveSubsystem() {
   }
 }
--- java/frc/robot/commands/Locomocao.java
+++ java/frc/robot/commands/Locomocao.java
@@ -3,6 +3,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 
+
+
 public class Locomocao extends Command {
   // Variáveis
   boolean botaoA, botaoB, botaoC, botaoD;
@@ -38,7 +40,6 @@ public class Locomocao extends Command {
   }
 
   public void stop(){
-    
   }
 
   @Override
